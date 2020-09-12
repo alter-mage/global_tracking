f5fdf8eef8ecfbef4ebaebb6123757d60367e057
Siddhant SRIVASTAVA <siddhant.srivastava@amadeus.com>
2020-09-12 20:31:39 +0530

    afkl auth fix

--- scripts/afkl_driver.py
+++ scripts/afkl_driver.py
@@ -3,6 +3,7 @@ import json
 import ssl
 import scripts.users
 import base64
+import requests
 
 auth_url = 'https://api-ute2-ext.airfranceklm.com/cargo/tracking/oauth'
 track_url = 'https://api-ute2-ext.airfranceklm.com/cargo/tracking/v2/public/shipments'
@@ -12,24 +13,24 @@ def get_access():
     user_base64 = base64.b64encode(
         ('%s:%s' % (user['username'], user['password'])).encode('ascii')
     ).decode('utf-8')
-
+    
     auth_req = urllib.request.Request(
         auth_url,
         headers={
             'content-type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic %s' % (user_base64)
+            'Authorization': 'Basic %s' % (user_base64),
+            'Cookie': 'BCSI-CS-5f9302ec8120e41e=1; CLID_cgobkgap_xte_=CZzuipnN+rvxhrxMBWqJMCuED4zhyxv+OKey2m8bbWli6iRF8aNLVLhsZ5RED9Wkh68oMfAAAAAB'
         },
         data = 'grant_type=client_credentials'.encode('utf8')
     )
-    print(auth_req.__dict__)
 
     auth_res = urllib.request.urlopen(auth_req, context=ssl._create_unverified_context())
     auth_data = json.loads(auth_res.read())
-    return auth_data['accessToken']
+    return auth_data['access_token']
 
 def track_shipment(track_request_data):
-    # access_token = get_access()
-    access_token = 'nduerk9acxqftrjux8qktvqb'
+    access_token = get_access()
+    # access_token = 'nduerk9acxqftrjux8qktvqb'
     print(access_token)
 
     req_awb = '/%s' % (track_request_data)
