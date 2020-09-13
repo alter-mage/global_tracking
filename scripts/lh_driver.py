6852a21ac69934d3c864b2fb7dda46dd8ec855d5
Siddhant Srivastava <sidsrivast42@gmail.com>
2020-09-13 17:06:30 +0530

    lh details

--- scripts/lh_driver.py
+++ scripts/lh_driver.py
@@ -1,11 +1,34 @@
 import urllib.request
-from flask import request
 import json
 import ssl
 import scripts.users
 
 auth_url = 'https://api.lufthansa.com/v1/oauth/token'
 track_url = 'https://api.lufthansa.com/v2/cargo/shipment/tracking'
+
+confirmed_status = ['BKG', 'FWB', 'FOH', 'RCS']
+transit_status = ['DEP', 'ARR', 'NFD', 'RCF']
+delivered_status = ['DLV']
+
+def process_response(track_response):
+    cargo_sos = []
+
+    for shipment_status in track_response:
+        # print(key)
+        # print(track_response[key])
+        cargo_info = {
+            'doc_number': track_response[shipment_status]['shipment']['shipmentId']['carrierNumericCode']+'-'+track_response['shipmentTrackingStatus']['shipment']['shipmentId']['aWBNumber'],
+            'origin': track_response[shipment_status]['booking']['origin'],
+            'destination': track_response[shipment_status]['booking']['destination']
+        }
+
+        cargo_confirmed_milestones = []
+        cargo_transit_milestones = []
+        cargo_delivered_milestones = []
+        cargo_milestone = {
+            'event_airport': track_response['eventAirport'],
+            'movement_details': cargo_tracking_status['movementDetails']
+        }
 
 def track_lh_shipment(track_request_data):
     access_token = 'b3btgpxkt2mr6ftwgwwgj73u'
